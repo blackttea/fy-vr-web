16684d82655d8c7ec02d04c75524a8a6a6ff3b59
wsj <shaojin.wang@vixtel.com>
2023-03-31 14:56:06 +0800

    feat: init

---
+++ src/view/system/layout/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, theme } from 'antd';
+import './index.scss'
+import FyMenu from "./menu";
+
+const { Header, Sider, Content } = Layout;
+
+const Lay: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className="lay-container">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <FyMenu />
+      </Sider>
+      <Layout className="site-layout">
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Lay;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,60 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path, { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      /** @ 符号指向 src 目录 */
+      "@": resolve(__dirname, "./src")
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"]
+  },
+  server: {
+    /** 是否开启 HTTPS */
+    https: false,
+    /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
+    host: true, // host: "0.0.0.0"
+    /** 端口号 */
+    port: 9999,
+    /** 是否自动打开浏览器 */
+    open: false,
+    /** 跨域设置允许 */
+    cors: true,
+    /** 端口被占用时，是否直接退出 */
+    strictPort: false,
+    /** 接口代理 */
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        // target: "https://mock.mengxuegu.com/mock/63218b5fb4c53348ed2bc212/api/v1",
+        ws: true,
+        /** 是否允许跨域 */
+        changeOrigin: true
+        // rewrite: (path) => path.replace("/api", "")
+      }
+    }
+  },
+  build: {
+    /** 消除打包大小超过 500kb 警告 */
+    chunkSizeWarningLimit: 2000,
+    /** Vite 2.6.x 以上需要配置 minify: "terser", terserOptions 才能生效 */
+    minify: "terser",
+    /** 在打包代码时移除 console.log、debugger 和 注释 */
+    terserOptions: {
+      compress: {
+        drop_console: false,
+        drop_debugger: true,
+        pure_funcs: ["console.log"]
+      },
+      format: {
+        /** 删除注释 */
+        comments: false
+      }
+    },
+    /** 打包后静态资源目录 */
+    assetsDir: "static"
+  }
 })
